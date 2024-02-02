d41e39614b31f3c1c5abe0a10e697e86b9e79009
Orchaldir <orchaldir@gmail.com>
2024-02-02 19:43:18 +0100

    Add Color

---
+++ rpg_tools_core/src/model/color.rs
@@ -0,0 +1,23 @@
+/// A color defined by a name.
+/// See https://en.wikipedia.org/wiki/Web_colors.
+#[derive(Default, Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Color {
+    Aqua,
+    Black,
+    Blue,
+    Fuchsia,
+    Gray,
+    Green,
+    Lime,
+    Maroon,
+    Navy,
+    Olive,
+    Orange,
+    Purple,
+    Red,
+    SaddleBrown,
+    Silver,
+    Teal,
+    White,
+    Yellow,
+}
