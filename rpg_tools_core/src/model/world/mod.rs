de8be4758e2d72d30002e249ee653c048cb4d996
Orchaldir <orchaldir@gmail.com>
2024-02-03 12:28:07 +0100

    Add River

--- rpg_tools_core/src/model/world/mod.rs
+++ rpg_tools_core/src/model/world/mod.rs
@@ -1,10 +1,13 @@
+use crate::model::world::river::{River, RiverId};
 use crate::model::world::town::{Town, TownId};
 use crate::utils::storage::Storage;
 
+pub mod river;
 pub mod town;
 
 /// Contains the terrain features & settlements.
 #[derive(Debug, Default)]
 pub struct WorldData {
+    pub river_manager: Storage<RiverId, River>,
     pub town_manager: Storage<TownId, Town>,
 }
