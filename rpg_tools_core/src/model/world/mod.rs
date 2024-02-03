bf07ad08cbef229db309352e1b7c59d65d88b696
Orchaldir <orchaldir@gmail.com>
2024-02-03 12:01:50 +0100

    Add WorldData

--- rpg_tools_core/src/model/world/mod.rs
+++ rpg_tools_core/src/model/world/mod.rs
@@ -1,18 +1,10 @@
-use crate::model::world::town::TownId;
+use crate::model::world::town::{Town, TownId};
 use crate::utils::storage::Storage;
 
 pub mod town;
 
 /// Contains the terrain features & settlements.
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct WorldData {
-    pub town_manager: Storage<TownId, TownId>,
-}
-
-impl Default for WorldData {
-    fn default() -> Self {
-        Self {
-            town_manager: Default::default(),
-        }
-    }
+    pub town_manager: Storage<TownId, Town>,
 }
