bd7d9e0ec4a737909c43422501787a1093b98715
Orchaldir <orchaldir@gmail.com>
2024-02-07 07:01:11 +0100

    Add street

---
+++ rpg_tools_core/src/model/world/street.rs
@@ -0,0 +1,49 @@
+use crate::utils::storage::{Element, Id};
+
+/// The unique identifier of a [`street`](Street).
+#[derive(Default, Copy, Clone, Debug, Eq, PartialEq, Hash)]
+pub struct StreetId(usize);
+
+impl Id for StreetId {
+    fn new(id: usize) -> Self {
+        Self(id)
+    }
+
+    fn id(&self) -> usize {
+        self.0
+    }
+}
+
+/// A street in the game.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Street {
+    id: StreetId,
+    name: String,
+}
+
+impl Street {
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+}
+
+impl Element<StreetId> for Street {
+    fn new(id: StreetId) -> Self {
+        Street {
+            id,
+            name: format!("Street {}", id.0),
+        }
+    }
+
+    fn id(&self) -> StreetId {
+        self.id
+    }
+
+    fn with_id(self, id: StreetId) -> Self {
+        Street { id, ..self }
+    }
+}
