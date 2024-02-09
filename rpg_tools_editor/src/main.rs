dd29dc4271a6e9a8efc4b3d582b275a851533efd
Orchaldir <orchaldir@gmail.com>
2024-02-09 20:26:49 +0100

    Add tenplates

--- rpg_tools_editor/src/main.rs
+++ rpg_tools_editor/src/main.rs
@@ -2,6 +2,8 @@
 extern crate rocket;
 
 use crate::init::init;
+use rocket::State;
+use rocket_dyn_templates::{context, Template};
 use rpg_tools_core::model::world::WorldData;
 
 mod init;
@@ -11,8 +13,16 @@ pub struct EditorData {
 }
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, world!"
+fn hello(data: &State<EditorData>) -> Template {
+    Template::render(
+        "home",
+        context! {
+            mountains: data.data.mountain_manager.get_all().len(),
+            rivers: data.data.river_manager.get_all().len(),
+            streets: data.data.street_manager.get_all().len(),
+            towns: data.data.town_manager.get_all().len(),
+        },
+    )
 }
 
 #[launch]
@@ -20,4 +30,5 @@ fn rocket() -> _ {
     rocket::build()
         .manage(EditorData { data: init() })
         .mount("/", routes![hello])
+        .attach(Template::fairing())
 }
