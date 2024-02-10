894d9ab35aa14a73507a4489f44a9c88cd476870
Orchaldir <orchaldir@gmail.com>
2024-02-10 10:33:45 +0100

    Add get_mountain_details()

--- rpg_tools_editor/src/route/mountain.rs
+++ rpg_tools_editor/src/route/mountain.rs
@@ -1,7 +1,7 @@
 use crate::EditorData;
 use rocket::State;
 use rocket_dyn_templates::{context, Template};
-use rpg_tools_core::model::world::mountain::{Mountain, MountainId};
+use rpg_tools_core::model::world::mountain::MountainId;
 use rpg_tools_core::utils::storage::{Element, Id};
 
 #[get("/mountain/all")]
@@ -11,18 +11,17 @@ pub fn get_all_mountains(data: &State<EditorData>) -> Template {
 
 #[get("/mountain/details/<id>")]
 pub fn get_mountain_details(data: &State<EditorData>, id: usize) -> Option<Template> {
-    data.data
-        .mountain_manager
-        .get(MountainId::new(id))
-        .map(|mountain| get_details_template(&data, id, mountain))
+    get_details_template(data, MountainId::new(id))
 }
 
-fn get_details_template(data: &EditorData, id: usize, mountain: &Mountain) -> Template {
-    Template::render(
-        "mountain/details",
-        context! {
-            name: mountain.name(),
-            id: id,
-        },
-    )
+fn get_details_template(data: &EditorData, id: MountainId) -> Option<Template> {
+    data.data.mountain_manager.get(id).map(|mountain| {
+        Template::render(
+            "mountain/details",
+            context! {
+                name: mountain.name(),
+                id: id.id(),
+            },
+        )
+    })
 }
