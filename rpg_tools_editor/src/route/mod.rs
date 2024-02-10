79c07b123c03a2c08be5c93bb0e8801c52812bc8
Orchaldir <orchaldir@gmail.com>
2024-02-10 07:53:09 +0100

    generic

--- rpg_tools_editor/src/route/mod.rs
+++ rpg_tools_editor/src/route/mod.rs
@@ -1,2 +1,26 @@
+use rocket_dyn_templates::{context, Template};
+use rpg_tools_core::utils::storage::{Element, Id, Storage};
+
 pub mod mountain;
 pub mod river;
+
+pub fn get_all_template<ID: Id, ELEMENT: Element<ID>>(
+    storage: &Storage<ID, ELEMENT>,
+    name: &str,
+    title: &str,
+) -> Template {
+    let values: Vec<(usize, &str)> = storage
+        .get_all()
+        .iter()
+        .map(|c| (c.id().id(), c.name()))
+        .collect();
+
+    Template::render(
+        "generic/all",
+        context! {
+            name: name,
+            title: title,
+            values: values,
+        },
+    )
+}
--- rpg_tools_editor/src/route/mountain.rs
+++ rpg_tools_editor/src/route/mountain.rs
@@ -1,26 +1,8 @@
 use crate::EditorData;
 use rocket::State;
-use rocket_dyn_templates::{context, Template};
-use rpg_tools_core::model::world::WorldData;
-use rpg_tools_core::utils::storage::{Element, Id};
+use rocket_dyn_templates::Template;
 
 #[get("/mountain/all")]
 pub fn get_all_mountains(data: &State<EditorData>) -> Template {
-    get_all_template(&data.data)
-}
-
-fn get_all_template(data: &WorldData) -> Template {
-    let mountains: Vec<(usize, &str)> = data
-        .mountain_manager
-        .get_all()
-        .iter()
-        .map(|c| (c.id().id(), c.name()))
-        .collect();
-
-    Template::render(
-        "mountain",
-        context! {
-            mountains: mountains,
-        },
-    )
+    crate::route::get_all_template(&data.data.mountain_manager, "mountain", "Mountains")
 }
