6704ca9cff792140a897a52e3d2fdc9181e87a40
Orchaldir <orchaldir@gmail.com>
2024-02-22 21:26:19 +0100

    update paths

--- rpg_tools_editor/src/route/town/tile.rs
+++ rpg_tools_editor/src/route/town/tile.rs
@@ -107,7 +107,7 @@ fn render_to_svg(renderer: &EdgeMapRenderer, town: &Town) -> RawSvg {
         &Point2d::default(),
         &town.map,
         TownTile::get_color,
-        |index, _tile| format!("../../edit/{}/{}", town.id().id(), index),
+        |index, _tile| uri!(edit_tile(town.id().id(), index)).to_string(),
     );
 
     let svg = builder.finish();
