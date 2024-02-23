9b0b7634bbfe415a4e977692b4727118d9d3c8e4
Orchaldir <orchaldir@gmail.com>
2024-02-23 22:41:27 +0100

    Improve create_building()

--- rpg_tools_core/src/usecase/create/building.rs
+++ rpg_tools_core/src/usecase/create/building.rs
@@ -1,17 +1,25 @@
 use crate::model::world::building::lot::BuildingLot;
 use crate::model::world::building::{Building, BuildingId};
+use crate::model::world::town::construction::Construction;
 use crate::model::world::WorldData;
-use crate::utils::storage::Id;
+use crate::utils::storage::{Element, Id};
 use anyhow::{bail, Result};
 
 /// Tries to a [`building`](Building).
 pub fn create_building(data: &mut WorldData, lot: BuildingLot) -> Result<BuildingId> {
-    if let Some(town) = data.town_manager.get(lot.town) {
-        if let Some(tile) = town.map.get_tile(lot.tile) {
-            return Ok(data.building_manager.create(|id| Building::new(id, lot)));
+    if let Some(town) = data.town_manager.get_mut(lot.town) {
+        if let Some(tile) = town.map.get_tile_mut(lot.tile) {
+            if tile.construction == Construction::None {
+                let id = data.building_manager.create(|id| Building::new(id, lot));
+                tile.construction = Construction::Building { id };
+
+                return Ok(id);
+            }
+        } else {
+            bail!("Tile {} is outside town {}!", lot.tile, town.name());
         }
 
-        bail!("Tile {} is outside town id {}!", lot.tile, lot.town.id());
+        bail!("Tile {} in town {} is occupied!", lot.tile, town.name(),);
     } else {
         bail!("Unknown town id {}!", lot.town.id());
     }
@@ -27,15 +35,11 @@ mod tests {
     #[test]
     fn create_successful() {
         let mut data = WorldData::default();
-        data.town_manager.create(Town::new);
+        let town_id = data.town_manager.create(Town::new);
 
         let id = create_building(&mut data, create_lot(0)).unwrap();
 
-        assert_eq!(id.id(), 0);
-        assert_eq!(
-            data.building_manager.get(id).unwrap(),
-            &Building::new(id, create_lot(0))
-        )
+        assert_first_building(&data, town_id, id);
     }
 
     #[test]
@@ -55,11 +59,38 @@ mod tests {
         assert!(data.building_manager.get_all().is_empty())
     }
 
+    #[test]
+    fn occupied_by_building() {
+        let mut data = WorldData::default();
+        let town_id = data.town_manager.create(Town::new);
+        let id = create_building(&mut data, create_lot(0)).unwrap();
+
+        assert_first_building(&data, town_id, id);
+    }
+
     fn create_lot(tile: usize) -> BuildingLot {
         BuildingLot {
             town: TownId::default(),
             tile,
             size: Size2d::square(1),
         }
+    }
+
+    fn assert_first_building(data: &WorldData, town_id: TownId, id: BuildingId) {
+        assert_eq!(id.id(), 0);
+        assert_eq!(
+            data.building_manager.get(id).unwrap(),
+            &Building::new(id, create_lot(0))
+        );
+        assert_eq!(
+            data.town_manager
+                .get(town_id)
+                .unwrap()
+                .map
+                .get_tile(0)
+                .unwrap()
+                .construction,
+            Construction::Building { id }
+        );
     }
 }
