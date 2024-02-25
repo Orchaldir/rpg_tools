56dd11b30d0eeda35cbf1c7f61f9f2811976f6f1
Orchaldir <orchaldir@gmail.com>
2024-02-25 14:58:47 +0100

    Add test

--- rpg_tools_core/src/usecase/edit/resize.rs
+++ rpg_tools_core/src/usecase/edit/resize.rs
@@ -16,3 +16,18 @@ pub fn resize_town(data: &mut WorldData, id: TownId, width: u32, height: u32) ->
         })
         .context("Town doesn't exist")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::model::world::town::TownId;
+    use crate::model::world::WorldData;
+
+    #[test]
+    fn resize_non_existing_town() {
+        let mut data = WorldData::default();
+
+        assert!(resize_town(&mut data, TownId::default(), 2, 3).is_err());
+        assert!(data.town_manager.get_all().is_empty());
+    }
+}
