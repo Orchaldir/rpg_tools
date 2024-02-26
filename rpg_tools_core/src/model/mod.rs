c89220d56389affd8466f8e36e82c0c7a7f8c7b2
Orchaldir <orchaldir@gmail.com>
2024-02-26 20:28:36 +0100

    Add RpgData

--- rpg_tools_core/src/model/mod.rs
+++ rpg_tools_core/src/model/mod.rs
@@ -1,3 +1,29 @@
+use crate::model::world::WorldData;
+use std::path::PathBuf;
+
 pub mod color;
 pub mod math;
 pub mod world;
+
+#[derive(Debug, Default)]
+pub struct RpgData {
+    pub setting: String,
+    pub world: WorldData,
+}
+
+impl RpgData {
+    pub fn empty(setting: String) -> Self {
+        RpgData {
+            setting,
+            ..RpgData::default()
+        }
+    }
+
+    pub fn get_path(&self, file: &str) -> PathBuf {
+        get_setting_path(&self.setting, file)
+    }
+}
+
+pub fn get_setting_path(setting: &str, file: &str) -> PathBuf {
+    ["resources", "settings", setting, file].iter().collect()
+}
