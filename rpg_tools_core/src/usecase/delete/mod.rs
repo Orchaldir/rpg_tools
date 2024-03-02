6412c9d9e278bdb9f31af4bc99e9b75a298bd4d8
Orchaldir <orchaldir@gmail.com>
2024-03-02 12:01:40 +0100

    Add DeleteResult

---
+++ rpg_tools_core/src/usecase/delete/mod.rs
@@ -0,0 +1,13 @@
+use crate::model::world::town::TownId;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum DeleteResult {
+    Ok,
+    NotFound,
+    Blocked(BlockingReason),
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+pub struct BlockingReason {
+    pub towns: Vec<TownId>,
+}
