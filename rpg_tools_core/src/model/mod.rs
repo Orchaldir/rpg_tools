fc33932a67341e25093c26bda56858aa39ae6eeb
Orchaldir <orchaldir@gmail.com>
2024-03-07 20:44:04 +0100

    Add characters

--- rpg_tools_core/src/model/mod.rs
+++ rpg_tools_core/src/model/mod.rs
@@ -1,3 +1,4 @@
+use crate::model::character::{Character, CharacterId};
 use crate::model::world::building::{Building, BuildingId};
 use crate::model::world::mountain::{Mountain, MountainId};
 use crate::model::world::river::{River, RiverId};
@@ -17,6 +18,7 @@ pub mod world;
 pub struct RpgData {
     pub setting: String,
     pub building_manager: Storage<BuildingId, Building>,
+    pub characters: Storage<CharacterId, Character>,
     pub mountain_manager: Storage<MountainId, Mountain>,
     pub river_manager: Storage<RiverId, River>,
     pub street_manager: Storage<StreetId, Street>,
@@ -28,6 +30,7 @@ impl RpgData {
         Ok(Self {
             setting: setting.to_string(),
             building_manager: load_storage(setting, "building")?,
+            characters: load_storage(setting, "character")?,
             mountain_manager: load_storage(setting, "mountain")?,
             river_manager: load_storage(setting, "river")?,
             street_manager: load_storage(setting, "street")?,
@@ -37,6 +40,7 @@ impl RpgData {
 
     pub fn save(&self) -> anyhow::Result<()> {
         save_storage(&self.building_manager, &self.setting)?;
+        save_storage(&self.characters, &self.setting)?;
         save_storage(&self.mountain_manager, &self.setting)?;
         save_storage(&self.river_manager, &self.setting)?;
         save_storage(&self.street_manager, &self.setting)?;
