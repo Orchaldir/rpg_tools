c334679162391d06391f88ed798cb45148e7e4a2
Orchaldir <orchaldir@gmail.com>
2024-03-07 20:08:48 +0100

    Fix

--- rpg_tools_core/src/model/character/gender.rs
+++ rpg_tools_core/src/model/character/gender.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 /// The gender of the [`character`](crate::model::character::Character).
-#[derive(Default, Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Default, Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub enum Gender {
     #[default]
     Female,
