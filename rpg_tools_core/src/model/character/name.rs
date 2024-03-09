1bdfba17e5455b4d436329cab0210faa9dc35f75
Orchaldir <orchaldir@gmail.com>
2024-03-09 09:46:42 +0100

    Add name

--- rpg_tools_core/src/model/character/name.rs
+++ rpg_tools_core/src/model/character/name.rs
@@ -1,5 +1,6 @@
 use crate::model::name::Name;
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 
 /// A name of a [`character`](crate::model::character::Character).
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
@@ -7,6 +8,44 @@ pub struct CharacterName {
     first: Name,
     middle: Option<Name>,
     last: Lastname,
+}
+
+impl CharacterName {
+    /// Returns a character name with middle name.
+    pub fn full(first: Name, middle: Name, last: Lastname) -> Self {
+        Self {
+            first,
+            middle: Some(middle),
+            last,
+        }
+    }
+
+    /// Returns a character name without middle name.
+    pub fn simple(first: Name, last: Lastname) -> Self {
+        Self {
+            first,
+            middle: None,
+            last,
+        }
+    }
+}
+
+impl Display for CharacterName {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.first.fmt(f)?;
+
+        if let Some(middle) = &self.middle {
+            write!(f, " {}", middle.to_string())?;
+        }
+
+        if let Lastname::Family(name) | Lastname::Patronymic(name) | Lastname::Matronymic(name) =
+            &self.last
+        {
+            write!(f, " {}", name.to_string())?;
+        }
+
+        Ok(())
+    }
 }
 
 /// The last name of a [`character`](crate::model::character::Character).
@@ -19,4 +58,28 @@ pub enum Lastname {
     Patronymic(Name),
     /// A last name based on the first name of the mother.
     Matronymic(Name),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn full_name_to_string() {
+        let first = Name::new("A").unwrap();
+        let middle = Name::new("B").unwrap();
+        let last = Lastname::Family(Name::new("C").unwrap());
+        let full = CharacterName::full(first, middle, last);
+
+        assert_eq!(full.to_string(), "A B C");
+    }
+
+    #[test]
+    fn simple_name_to_string() {
+        let first = Name::new("First").unwrap();
+        let last = Lastname::Family(Name::new("Last").unwrap());
+        let simple = CharacterName::simple(first, last);
+
+        assert_eq!(simple.to_string(), "First Last");
+    }
 }
--- rpg_tools_core/src/model/name.rs
+++ rpg_tools_core/src/model/name.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
 
 /// A valid name.
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
@@ -14,8 +15,8 @@ impl Name {
     ///
     /// assert_eq!(Name::new(""), None);
     /// assert_eq!(Name::new("  "), None);
-    /// assert_eq!(Name::new("Test0").unwrap().str(), "Test0");
-    /// assert_eq!(Name::new("  Test1  ").unwrap().str(), "Test1");
+    /// assert_eq!(Name::new("Test0").unwrap().to_string(), "Test0");
+    /// assert_eq!(Name::new("  Test1  ").unwrap().to_string(), "Test1");
     /// ```
     pub fn new<S: Into<String>>(name: S) -> Option<Name> {
         let trimmed = name.into().trim().to_string();
@@ -26,8 +27,10 @@ impl Name {
             Some(Self { text: trimmed })
         }
     }
+}
 
-    pub fn str(&self) -> &str {
-        &self.text
+impl Display for Name {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.text)
     }
 }
